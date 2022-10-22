5e19a10d66c4d0124a21aef049e26243a5c4df1b
Defi Tamara <defytamara2610@gmail.com>
2022-10-22 16:09:28 +0700

    Landing Page / Frontend
    - Belum terhubung dengan database
    - Login berhasil

--- makrifatbusiness/routes/web.php
+++ makrifatbusiness/routes/web.php
@@ -17,12 +17,13 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('frontend.home');
 });
 
 
 Auth::routes();
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('dashboard', [App\Http\Controllers\HomeController::class, 'dashboard'])->name('dashboard')->middleware('is_admin');
 
 Route::group(['namespace' => 'Backend'], function()
